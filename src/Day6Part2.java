3285c335a9fe2ef53af2b26161d42d114ec8a412
unknown <okaci25@gmail.com>
2023-01-10 00:20:05 +0100

    Day 6 part1 & 2 done

---
+++ src/Day6Part2.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
+
+
+public class Day6Part2 {
+    public static void main(String[] args) throws Exception{
+
+        @SuppressWarnings("resource")
+		BufferedReader br = new BufferedReader(new FileReader("inputs/Day6.txt"));
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] characters = line.split("");
+
+            List<String> buffer = new ArrayList<>();
+
+            int idx = 0;
+            for (String s : characters) {
+                if (buffer.size() > 13) {
+                    if (buffer.size() == new HashSet<>(buffer).size()) {
+                        break;
+                    }
+                    buffer.add(s);
+                    buffer.remove(0);
+                } else {
+                    buffer.add(s);
+                }
+                idx++;
+            }
+
+            System.out.println(idx);
+        }
+    }
+}
