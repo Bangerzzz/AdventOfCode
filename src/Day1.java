b39b14c3b18feae17aea0479ab2ece748e40edb4
unknown <okaci25@gmail.com>
2023-01-09 22:59:15 +0100

    Day1 done

--- src/Day1.java
+++ src/Day1.java
@@ -1,27 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Day1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		
-	    String calories = """
-	1000
-	2000
-	3000
+	    @SuppressWarnings("resource")
+		BufferedReader br = new BufferedReader (new FileReader("inputs/Day1.txt"));
+	    
+	    int calorie = 0;
+	    int mostCalories = 0;
+	    List<Integer> calorieList = new ArrayList<>();
+	    String line;
+	    
+	    while ((line = br.readLine()) != null) {
+            String strCal = line.trim();
+            if (strCal.isBlank()) {
+                if (mostCalories < calorie) {
+                    mostCalories = calorie;
+                }
+                calorieList.add(calorie);
+                calorie = 0;
+                continue;
+            }
+            calorie += Integer.parseInt(strCal);
+        }
+        calorieList.add(calorie);
+        if (mostCalories < calorie) {
+            mostCalories = calorie;
+        }
 
-	4000
+        System.out.println(mostCalories);
 
-	5000
-	6000
-
-	7000
-	8000
-	9000
-
-	10000
-	                       """;
-	    for(String s: calories.split("\n")){
-	        System.out.println(s);
-	    }
+        calorieList.sort((a, b) -> b - a); //Decroissante
+        System.out.println(calorieList.get(0) + calorieList.get(1) + calorieList.get(2));
 	}
 
 }
